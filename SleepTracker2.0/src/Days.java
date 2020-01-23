f35397f3bc5e7e4418d66f8069873489f95bd985
s-tzenc <s-tzenc@53S500036926.it.bsd405.org>
2020-01-23 14:09:26 -0800

    fixed days window

--- SleepTracker2.0/src/Days.java
+++ SleepTracker2.0/src/Days.java
@@ -44,7 +44,7 @@ public class Days {
 	}
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 765, 510);
+		frame.setBounds(100, 100, 767, 344);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -52,32 +52,23 @@ public class Days {
 		table.setModel(new DefaultTableModel(
 			new Object[][] {
 				{"Measurements", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
-				{null, null, null, null, null, null, null, null},
+				{"Average hrs", null, null, null, null, null, null, null},
+				{"Hrs for this week", null, null, null, null, null, null, null},
+				{"Meet rec'd hours?", null, null, null, null, null, null, null},
+				{"Total hrs this week", null, null, null, null, null, null, null},
 			},
 			new String[] {
 				"Measurements", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
 			}
 		));
+		table.getColumnModel().getColumn(0).setPreferredWidth(90);
+		table.getColumnModel().getColumn(3).setPreferredWidth(76);
 		if(input) {
 			table.setValueAt(hours, 1, column);
 		}
 		table.setRowHeight(43);
-		table.getColumnModel().getColumn(0).setPreferredWidth(94);
-		table.getColumnModel().getColumn(3).setPreferredWidth(76);
-		table.setBounds(0, 76, 747, 387);
+		table.setBounds(0, 82, 749, 215);
 		frame.getContentPane().add(table);
-		
-		JLabel lblNewLabel = new JLabel("Your Stats for this Week:", SwingConstants.CENTER);
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 55));
-		lblNewLabel.setBounds(98, 0, 659, 81);
-		frame.getContentPane().add(lblNewLabel);
 		
 		button = new JButton("<-");
 		button.addActionListener(new ActionListener() {
@@ -87,8 +78,13 @@ public class Days {
 				frame.dispose();
 			}
 		});
-		button.setBounds(0, 0, 97, 25);
+		button.setBounds(0, 0, 56, 25);
 		frame.getContentPane().add(button);
+		
+		JLabel lblNewLabel = new JLabel("Your Stats for this Week:", SwingConstants.CENTER);
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 55));
+		lblNewLabel.setBounds(0, 0, 747, 81);
+		frame.getContentPane().add(lblNewLabel);
 		
 		JButton btnRecordTime = new JButton("Record Time");
 		btnRecordTime.addActionListener(new ActionListener() {
@@ -98,7 +94,7 @@ public class Days {
 				frame.dispose();
 			}
 		});
-		btnRecordTime.setBounds(0, 45, 97, 25);
+		lblNewLabel.setBounds(0, 0, 747, 81);
 		frame.getContentPane().add(btnRecordTime);
 	}	
 	
